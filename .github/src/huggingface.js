71a5c6e05cf5e4255a89bcb0ab2e06d5021635de
Oliver Varney <okv3372@g.rit.edu>
2024-11-09 17:55:30 -0500

    new model

--- .github/src/huggingface.js
+++ .github/src/huggingface.js
@@ -2,7 +2,7 @@ const { pipeline } = require('@huggingface/transformers');
 
 async function main() {
     try {
-        const pipe = await pipeline('summarization');
+        const pipe = await pipeline('summarization', 'Xenova/t5-small');
         const out = await pipe('this meeting is useless because we are talking about nonsense');
 
         console.log(out);
