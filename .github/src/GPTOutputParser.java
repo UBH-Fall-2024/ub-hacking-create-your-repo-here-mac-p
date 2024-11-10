cbde437ad1ec01941f895ffb92330f3b7f466383
calebgiardino <caleb.giardino@gmail.com>
2024-11-10 02:02:04 -0500

    Rollback GPTOutputParser

--- .github/src/GPTOutputParser.java
+++ .github/src/GPTOutputParser.java
@@ -4,30 +4,26 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-
-
 public class GPTOutputParser {
-    //reads file from the AI in line by line and sends it to a parser that will interpret the results
     public static void readLine(String filename) throws IOException {
         try(BufferedReader br = new BufferedReader(new FileReader(Path.of(filename).toFile()))) {
             String line = br.readLine();
             while (line != null) {
-                //calls the parsing function
-                parser(line);
-                //skips to the next line
+                String result = parser(line);
                 line = br.readLine();
             }
         }
     }
 
 
-public static String parser(String line) {
+    public static String parser(String line) {
 
         ArrayList<String> meetingQualities = new ArrayList<>();
 
-        //the final output string that will hold the results
+//       Gson gson = new Gson();
+//        JsonObject jsonObject = gson.fromJson(line, JsonObject.class);
+//        String input = jsonObject.toString();
         String finalOut = "";
-
         // Add 2 very bad qualities
         meetingQualities.add("Harassment");
         meetingQualities.add("Disrespectful behavior");
@@ -43,8 +39,8 @@ public static String parser(String line) {
         meetingQualities.add("Collaborating with others");
 
 
-//gets the information from the input line passed in by the AI and adds the information to a more readable string
-    ArrayList<String> temp = new ArrayList<>(Arrays.asList(line.split(":")));
+
+        ArrayList<String> temp = new ArrayList<>(Arrays.asList(line.split(":")));
         String name = temp.get(1).substring(1);
         String output = line.substring(line.lastIndexOf("[") + 1, line.lastIndexOf("]"));
         ArrayList<String> wordList = new ArrayList<>(Arrays.asList(output.split(",")));
@@ -53,16 +49,14 @@ public static String parser(String line) {
         for (int i = 0; i < meetingQualities.size(); i++) {
             finalOut += meetingQualities.get(i) + ": " + wordList.get(i) + '\n';
         }
-
-        //prints the final output
-    System.out.println(finalOut);
+        System.out.println(finalOut);
         return finalOut;
     }
 
     public static void main(String[] args) throws IOException {
-    try {readLine(".github/src/gptOutputString.txt");} catch (IOException e) {
-        throw new RuntimeException(e);
-    }
+        try {readLine(".github/src/gptOutputString.txt");} catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
