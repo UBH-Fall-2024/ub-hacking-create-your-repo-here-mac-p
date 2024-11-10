0e87eed3847964e68b31066ee32c0f451accbc0d
calebgiardino <caleb.giardino@gmail.com>
2024-11-09 21:46:13 -0500

    Added output parsing framework

--- .github/src/GPTOutputParser.java
+++ .github/src/GPTOutputParser.java
@@ -1,2 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class GPTOutputParser {
+    public static void main(String[] args) {
+        String input = "";
+        String output = "";
+        HashMap<String, String> emotionList = new HashMap<>();
+        for (Map.Entry<String, String> entry: emotionList.entrySet()){
+            output += entry.getKey() + ": " + entry.getValue();
+        }
+    }
+
 }
