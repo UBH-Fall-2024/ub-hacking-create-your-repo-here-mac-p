b80423eb782f127f8cc97a616335b6d8e44b01eb
Oliver Varney <okv3372@g.rit.edu>
2024-11-09 22:16:32 -0500

    added ai

--- .github/src/huggingface.js
+++ .github/src/huggingface.js
@@ -3,8 +3,16 @@ const { pipeline } = require('@huggingface/transformers');
 async function main() {
     try {
 
+        const fs = require('fs');
+
         const pipe = await pipeline('zero-shot-classification');
-        const out = await pipe('This is coming from a business meeting about helping customers: "John: we should be nice to customers"', ['business strategy', 'customer service', 'team collaboration', 'technical improvement'])
+        const out = await pipe('This is coming from a business meeting about helping customers: "John: I hate our customers!"', ['business strategy', 'customer service', 'team collaboration', 'technical improvement'])
+
+        const stringRepresentation = JSON.stringify(out);
+        fs.appendFile('gptOutputString.txt', stringRepresentation + '\n', (err) => {
+            if (err) throw err;
+            console.log('File has been written');
+        })
 
         console.log(out);
 
